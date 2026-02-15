43fdd103fee9c7383b49c36c26e658736c1654c5
shazahisham <sy2300650@tkh.edu.eg>
2026-02-15 15:32:04 +0000

    step 2

--- task1_shell.c
+++ task1_shell.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
 
 int main()
 {
     char in[50];
     char dir[50];
+    char *a[10];
 
     while(1)
     {
@@ -13,6 +15,20 @@ int main()
         fflush(stdout);
 
         fgets(in,50,stdin);
+        in[strcspn(in,"\n")] = 0;
+
+        int i=0;
+        char *t = strtok(in," ");
+        while(t){ a[i++] = t; t = strtok(NULL," "); }
+        a[i] = 0;
+
+        if(a[0] == 0) continue;
+
+        if(strcmp(a[0],"cd")==0)
+        {
+            if(a[1]==0) chdir(getcwd(dir,50));
+            else chdir(a[1]);
+        }
     }
 
     return 0;
