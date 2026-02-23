9a7e5cfb2eea6d5361a2c2fef19fe25d45acbe7c
shazahisham <sy2300650@tkh.edu.eg>
2026-02-23 15:28:37 +0000

    step 2

--- word_count.cpp
+++ word_count.cpp
@@ -2,8 +2,19 @@
 #include <fstream>
 #include <vector>
 #include <string>
+#include <unordered_map>
+#include<sstream>
 using namespace std;
 
+string cleanWord(string word) {
+    string result = "";
+    for (int i = 0; i < word.size(); i++) {
+        if (isalpha(word[i])) {
+            result += tolower(word[i]);
+        }
+    }
+    return result;
+}
 int main() {
     ifstream file("input.txt");
     vector<string> lines;
@@ -16,6 +27,22 @@ int main() {
     for (string l : lines) {
         cout << l << endl;
     }
+    unordered_map<string,int> wordCount;
+
+for (string l : lines) {
+    stringstream ss(l);
+    string word;
+    while (ss >> word) {
+        word = cleanWord(word);
+        if (word != "") {
+            wordCount[word]++;
+        }
+    }
+}
+
+for (auto pair : wordCount) {
+    cout << pair.first << " " << pair.second << endl;
+}
 
     return 0;
 }
