6c06265b9f51a74a0d6ccd1dde4e2c62581ad3e5
shazahisham <sy2300650@tkh.edu.eg>
2026-03-02 14:05:39 +0000

    step 2

--- main.cpp
+++ main.cpp
@@ -35,3 +35,57 @@ vector<int> readReferences(string filename) {
     file.close();
     return references;
 }
+int simulateAging(vector<int> &references, int numFrames) {
+    vector<Frame> frames(numFrames);
+    int pageFaults = 0;
+
+    for (int i = 0; i < references.size(); i++) {
+        int page = references[i];
+        bool hit = false;
+
+        for (int j = 0; j < numFrames; j++) {
+            if (frames[j].pageNumber == page) {
+                frames[j].referenceBit = true;
+                hit = true;
+                break;
+            }
+        }
+
+        if (!hit) {
+            pageFaults++;
+            int victim = 0;
+
+            for (int j = 0; j < numFrames; j++) {
+                if (frames[j].pageNumber == -1) {
+                    victim = j;
+                    break;
+                }
+            }
+
+            if (frames[victim].pageNumber != -1) {
+                int minCounter = frames[0].counter;
+                victim = 0;
+                for (int j = 1; j < numFrames; j++) {
+                    if (frames[j].counter < minCounter) {
+                        minCounter = frames[j].counter;
+                        victim = j;
+            }
+            }
+            }
+
+            frames[victim].pageNumber = page;
+            frames[victim].counter = 0;
+            frames[victim].referenceBit = true;
+        }
+
+        for (int j = 0; j < numFrames; j++) {
+            frames[j].counter >>= 1;
+            if (frames[j].referenceBit) {
+                frames[j].counter |= 128;
+                frames[j].referenceBit = false;
+    }
+    }
+    }
+
+    return pageFaults;
+}
