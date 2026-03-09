bad7cd5eace7afb60da8feac7a02a40fafd9c0c5
shazahisham <sy2300650@tkh.edu.eg>
2026-03-09 11:09:53 +0000

    step 5

--- detection.cpp
+++ detection.cpp
@@ -13,14 +13,24 @@ int main() {
     vector<int> A(m);
         for (int i = 0; i < m; i++) {
         file >> E[i];
-    }
+}
         for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
             file >> C[i][j];
         }
-    }
+}
         for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
             file >> R[i][j];
         }
-    }
+}
+    for (int j = 0; j < m; j++) {
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += C[i][j];
+        }
+        A[j] = E[j] - sum;
+}
+    vector<bool> finish(n, false);
+    bool progress = true;
+    
