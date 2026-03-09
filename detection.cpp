6f845f3c3a50c55a7dea7d72d621f1b1a7288cf6
shazahisham <sy2300650@tkh.edu.eg>
2026-03-09 11:07:23 +0000

    step 4

--- detection.cpp
+++ detection.cpp
@@ -11,3 +11,16 @@ int main() {
     vector<vector<int>> C(n, vector<int>(m));
     vector<vector<int>> R(n, vector<int>(m));
     vector<int> A(m);
+        for (int i = 0; i < m; i++) {
+        file >> E[i];
+    }
+        for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            file >> C[i][j];
+        }
+    }
+        for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            file >> R[i][j];
+        }
+    }
