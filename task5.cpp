7c40912f2b4ab41785fb84356215360d2ab7dae3
shazahisham <sy2300650@tkh.edu.eg>
2026-03-23 11:44:14 +0000

    step 2

--- task5.cpp
+++ task5.cpp
@@ -16,3 +16,36 @@ float FCFS(vector<int> arrival, vector<int> burst, int n) {
 
     return total / n;
 }
+float SJF(vector<int> arrival, vector<int> burst, int n) {
+    vector<bool> done(n, false);
+    vector<int> waiting(n, 0);
+
+    int completed = 0, time = 0;
+
+    while (completed < n) {
+        int idx = -1, minBurst = 1e9;
+
+        for (int i = 0; i < n; i++) {
+            if (!done[i] && arrival[i] <= time && burst[i] < minBurst) {
+                minBurst = burst[i];
+                idx = i;
+            }
+        }
+
+        if (idx == -1) {
+            time++;
+            continue;
+        }
+
+        waiting[idx] = time - arrival[idx];
+        time += burst[idx];
+        done[idx] = true;
+        completed++;
+    }
+
+    float total = 0;
+    for (int i = 0; i < n; i++)
+        total += waiting[i];
+
+    return total / n;
+}
