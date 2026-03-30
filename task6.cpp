e4b1504551fb9a5530fdc68b27dd8d7584c7de4c
shazahisham <sy2300650@tkh.edu.eg>
2026-03-30 21:21:16 +0200

    step1

---
+++ task6.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <filesystem>
+#include <map>
+
+using namespace std;
+using namespace std::filesystem;
+
+int main(int argc, char* argv[]) {
+
+    if (argc != 3) {
+        cout << "Usage: program <directory_path> <bin_width>\n";
+        return 1;
+    }
+
+    string path = argv[1];
+    long long binWidth = stoll(argv[2]);
+
+    if (!exists(path) || !is_directory(path)) {
+        cout << "Invalid directory.\n";
+        return 1;
+    }
+
+    if (binWidth <= 0) {
+        cout << "Bin width must be positive.\n";
+        return 1;
+    }
