03f61456f73f35c232fc6e6784df14623e606869
shazahisham <sy2300650@tkh.edu.eg>
2026-04-06 17:27:33 +0000

    step 3

--- client.cpp
+++ client.cpp
@@ -23,3 +23,36 @@ void receiveMessages(int sock) {
         cout << "> ";
     }
 }
+int main() {
+    int clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+
+    sockaddr_in serverAddr{};
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(54000);
+    inet_pton(AF_INET, "127.0.0.1", &serverAddr.sin_addr);
+
+    connect(clientSocket, (sockaddr*)&serverAddr, sizeof(serverAddr));
+
+    string name;
+    cout << "Enter your name: ";
+    getline(cin, name);
+    name += "\n";
+
+    send(clientSocket, name.c_str(), name.length(), 0);
+
+    thread t(receiveMessages, clientSocket);
+
+    string msg;
+    while (true) {
+        cout << "> ";
+        getline(cin, msg);
+
+        if (msg == "/quit") break;
+
+        msg += "\n";
+        send(clientSocket, msg.c_str(), msg.length(), 0);
+    }
+
+    close(clientSocket);
+    return 0;
+}
