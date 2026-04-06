e09865a3e5c2bcde40878cf20763f8b51530d653
shazahisham <sy2300650@tkh.edu.eg>
2026-04-06 17:26:45 +0000

    step2

--- client.cpp
+++ client.cpp
@@ -7,3 +7,19 @@
 #include <unistd.h>
 
 using namespace std;
+void receiveMessages(int sock) {
+    char buffer[1024];
+
+    while (true) {
+        int bytes = recv(sock, buffer, sizeof(buffer) - 1, 0);
+
+        if (bytes <= 0) {
+            cout << "\nDisconnected from server.\n";
+            break;
+        }
+
+        buffer[bytes] = '\0';
+        cout << "\n" << buffer;
+        cout << "> ";
+    }
+}
