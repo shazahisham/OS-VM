72d254f3ece8a9afff7826aede731a712a03e33c
shazahisham <sy2300650@tkh.edu.eg>
2026-04-13 19:26:52 +0200

    Update task8.cpp
    step2

--- task8.cpp
+++ task8.cpp
@@ -40,3 +40,60 @@ void makeKey(string password, DES_cblock &key) {
         }
     }
 }
+
+PasswordRecord encryptPassword(string password, unsigned short salt) {
+    DES_cblock key;
+    makeKey(password, key);
+
+    DES_set_odd_parity(&key);
+
+    DES_key_schedule schedule;
+    DES_set_key_unchecked(&key, &schedule);
+
+    DES_cblock block;
+    DES_cblock output;
+
+    unsigned char low = salt & 0xFF;
+    unsigned char high = (salt >> 8) & 0xFF;
+
+
+    for (int i = 0; i < 8; i++) {
+        if (i % 2 == 0) {
+            block[i] = low;
+        } else {
+            block[i] = high;
+        }
+    }
+
+    
+    for (int round = 0; round < 25; round++) {
+        block[0] ^= low;
+        block[1] ^= high;
+        block[2] ^= (low >> 1) | (high << 7);
+        block[3] ^= (high >> 1) | (low << 7);
+        block[4] ^= low ^ high;
+        block[5] ^= low + round;
+        block[6] ^= high + round;
+        block[7] ^= (low ^ high ^ round);
+
+        DES_ecb_encrypt(&block, &output, &schedule, DES_ENCRYPT);
+
+        for (int j = 0; j < 8; j++) {
+            block[j] = output[j];
+        }
+    }
+
+    PasswordRecord record;
+    record.salt = salt;
+    record.hash = toHex(output, 8);
+
+    return record;
+}
+
+
+string storePassword(PasswordRecord record) {
+    stringstream ss;
+    ss << hex << setw(4) << setfill('0') << record.salt;
+    ss << ":" << record.hash;
+    return ss.str();
+}
